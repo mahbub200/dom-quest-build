d6760139e4831c9340a777537bb12ad6230a5537
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-09-24 05:01:02 +0600

    making code dry fnctionalities

--- js/donation.js
+++ js/donation.js
@@ -3,13 +3,56 @@ document
   .getElementById("donate-for-feni")
   .addEventListener("click", function () {
     console.log("ddd");
-    const feniAmountInput = setElementByValue("input-feni-amount");
-    const accountBalance = setElementByIdText("account-balance");
-    let feniTotalAmount = setElementByIdText("feni-total-amount");
-    const overallFeniAmount = feniTotalAmount + feniAmountInput;
-    document.getElementById("feni-total-amount").innerText = overallFeniAmount;
-    const remainingBalance = accountBalance - feniAmountInput;
-    document.getElementById("account-balance").innerText = remainingBalance;
+    calculateAmount(
+      "input-feni-amount",
+      "account-balance",
+      "feni-total-amount"
+    );
+    // const feniAmountInput = setElementByValue("input-feni-amount");
+    // const accountBalance = setElementByIdText("account-balance");
+    // let feniTotalAmount = setElementByIdText("feni-total-amount");
+    // const overallFeniAmount = feniTotalAmount + feniAmountInput;
+    // document.getElementById("feni-total-amount").innerText = overallFeniAmount;
+    // const remainingBalance = accountBalance - feniAmountInput;
+    // document.getElementById("account-balance").innerText = remainingBalance;
 
-    console.log(overallFeniAmount);
+    // console.log(overallFeniAmount);
+  });
+document
+  .getElementById("donate-for-noakhali")
+  .addEventListener("click", function () {
+    console.log("ddd");
+    calculateAmount(
+      "input-noakhali-amount",
+      "account-balance",
+      "noakhali-total-amount"
+    );
+    // const feniAmountInput = setElementByValue("input-feni-amount");
+    // const accountBalance = setElementByIdText("account-balance");
+    // let feniTotalAmount = setElementByIdText("feni-total-amount");
+    // const overallFeniAmount = feniTotalAmount + feniAmountInput;
+    // document.getElementById("feni-total-amount").innerText = overallFeniAmount;
+    // const remainingBalance = accountBalance - feniAmountInput;
+    // document.getElementById("account-balance").innerText = remainingBalance;
+
+    // console.log(overallFeniAmount);
+  });
+document
+  .getElementById("donate-for-quota")
+  .addEventListener("click", function () {
+    console.log("ddd");
+    calculateAmount(
+      "input-quota-amount",
+      "account-balance",
+      "quota-total-amount"
+    );
+    // const feniAmountInput = setElementByValue("input-feni-amount");
+    // const accountBalance = setElementByIdText("account-balance");
+    // let feniTotalAmount = setElementByIdText("feni-total-amount");
+    // const overallFeniAmount = feniTotalAmount + feniAmountInput;
+    // document.getElementById("feni-total-amount").innerText = overallFeniAmount;
+    // const remainingBalance = accountBalance - feniAmountInput;
+    // document.getElementById("account-balance").innerText = remainingBalance;
+
+    // console.log(overallFeniAmount);
   });
--- js/utilities.js
+++ js/utilities.js
@@ -8,3 +8,23 @@ function setElementByValue(id) {
   const parseTextValue = parseInt(textValue);
   return parseTextValue;
 }
+
+function calculateAmount(id, accountBalance, totalAmount) {
+  // const feniAmountInput = setElementByValue("input-feni-amount");
+  const regionAmountInput = setElementByValue(id);
+  //   const accountBalance = setElementByIdText("account-balance");
+  const currentBalance = setElementByIdText(accountBalance);
+  //   let feniTotalAmount = setElementByIdText("feni-total-amount");
+  let regionTotalAmount = setElementByIdText(totalAmount);
+  //   const overallFeniAmount = feniTotalAmount + feniAmountInput;
+  const overallRegionAmount = regionAmountInput + regionTotalAmount;
+  //   document.getElementById("feni-total-amount").innerText = overallFeniAmount;
+  document.getElementById(totalAmount).innerText = overallRegionAmount;
+  //   const remainingBalance = accountBalance - feniAmountInput;
+  const remainingBalance = currentBalance - regionAmountInput;
+  //   document.getElementById("account-balance").innerText = remainingBalance;
+  document.getElementById(accountBalance).innerText = remainingBalance;
+  console.log(remainingBalance);
+
+  console.log(overallFeniAmount);
+}
