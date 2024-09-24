4dd003aca0ae4618e1c56b8fdd9246f8ca72ee1f
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-09-24 14:26:21 +0600

    validation completed

--- js/utilities.js
+++ js/utilities.js
@@ -11,20 +11,35 @@ function setElementByValue(id) {
 
 function calculateAmount(id, accountBalance, totalAmount) {
   // const feniAmountInput = setElementByValue("input-feni-amount");
-  const regionAmountInput = setElementByValue(id);
+  const regionAmountInputWithoutParsing = document.getElementById(id).value;
+  const regionAmountInput = parseInt(regionAmountInputWithoutParsing);
+
+  console.log(typeof regionAmountInput);
   //   const accountBalance = setElementByIdText("account-balance");
   const currentBalance = setElementByIdText(accountBalance);
   //   let feniTotalAmount = setElementByIdText("feni-total-amount");
   let regionTotalAmount = setElementByIdText(totalAmount);
   //   const overallFeniAmount = feniTotalAmount + feniAmountInput;
+  if (isNaN(regionAmountInput) || regionAmountInput < 0) {
+    alert("Please enter a valid, non-negative donation amount.");
+    return;
+  } else if (regionAmountInput > currentBalance) {
+    alert("Not enough balance in your account.so donate less");
+    return;
+  }
   const overallRegionAmount = regionAmountInput + regionTotalAmount;
+
   //   document.getElementById("feni-total-amount").innerText = overallFeniAmount;
   document.getElementById(totalAmount).innerText = overallRegionAmount;
   //   const remainingBalance = accountBalance - feniAmountInput;
   const remainingBalance = currentBalance - regionAmountInput;
+
   //   document.getElementById("account-balance").innerText = remainingBalance;
   document.getElementById(accountBalance).innerText = remainingBalance;
   console.log(remainingBalance);
 
-  console.log(overallFeniAmount);
+  // console.log(overallFeniAmount);
+  let name = "ffg";
+  let parsed = parseInt(name);
+  console.log(typeof parsed);
 }
